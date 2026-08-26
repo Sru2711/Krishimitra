a010776429269314af81596937359891559d5477
Sru2711 <srushtimalagi@gmail.com>
2026-08-26 18:34:30 +0530

    Configure standalone Next.js deployment

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
 import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   output: "standalone",
-//   // devIndicators: false,
-// };
-
-// export default nextConfig;
-
-
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  devIndicators: false, 
+  output: "standalone",
 };
 
 export default nextConfig;
